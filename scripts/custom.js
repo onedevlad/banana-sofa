9cc437386a28120c8958651082369ff46a3355af
Vlad Trishch <vlad.trishch@gmail.com>
2016-07-15 21:48:45 +0300

    Added a video. Working at slider

---
+++ scripts/custom.js
@@ -0,0 +1,44 @@
+var video = {
+	resettingDelay: 5,
+	resetting: 0,
+	running: false,
+}
+
+$.fn.isInView = function() {
+	return this[0].getBoundingClientRect().top >= 0 &&
+				 this[0].getBoundingClientRect().bottom <= window.innerHeight
+}
+
+
+$(document).ready(function(){ // Video module
+	var $video = $('video')
+
+	function videoClick(){
+		clearTimeout(video.resetting)
+		video.running ? pause($video[0]) : play($video[0])
+	}
+	function play(){
+		$video[0].play()
+		video.running = true
+		clearTimeout(video.resetting)
+	}
+	function pause(){
+		$video[0].pause()
+		video.running = false
+		clearTimeout(video.resetting)
+	}
+
+	$('body').scroll(function(){
+		if($video.isInView()) play()
+		else pause()
+	})
+
+	$('video').click(videoClick)
+})
+
+
+$(document).ready(function(){ // Slider module
+	$('.slick-slider').slick({
+		accessibility: true,
+	})
+})
